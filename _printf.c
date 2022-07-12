8321b975545228c15e08f21a3d31cbd5356ab9cd
MercyChebet <chebetm96@gmial.com>
2022-07-12 02:04:38 -0700

    More changes

--- _printf.c
+++ _printf.c
@@ -1,139 +1,46 @@
+#include <unistd.h>
 #include "main.h"
 /**
- * _printf - prints anything (output) according to a format
- * @format: character string (c, s, %)
- * Return: numbers of characters printed.
+ * _printf - takes in a string and prints different types of arguments for
+ * an unspecified amount of arguments
+ * @format: the initial string that tell us what is going to be printed
+ * Return: the amount of times we write to stdout
  */
-
 int _printf(const char *format, ...)
 {
+	int i, count;
+
+	int (*f)(va_list);
+
 	va_list list;
-	int i = 0, index = 0, count = 0, (*gof)(va_list list);
 
-	va_start(list, format);
-
-	if ((format == NULL) || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
 
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		index = i + 1;
-		if (format[i] != '%')
+	va_start(list, format);
+	i = count = 0;
 
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+				return (-1);
+			f = get_func(format[i + 1]);
+			if (f == NULL)
+				count += print_nan(format[i], format[i + 1]);
+			else
+				count += f(list);
+			i++;
+		}
+		else
 		{
 			_putchar(format[i]);
 			count++;
 		}
-		else if (format[i] == '%' && format[index] == '%')
-		{
-			_putchar('%');
-			i = index;
-			count++;
-		}
-		else
-		{
-			gof = get_op_func(&format[index]);
-			if (gof != NULL)
-			{
-				count += gof(list);
-				i = index;
-			}
-			else
-			{
-				_putchar(format[i]);
-				count++;
-			}
-		}
+		i++;
 	}
 	va_end(list);
 	return (count);
 }
 
-/**
- * op_char - Function that prints char
- * @list: Variable type va_list
- * Return: 0
- */
-int op_char(va_list list)
-{
-	_putchar(va_arg(list, int));
-	return (1);
-}
-
-/**
- * op_string - Function that prints string
- * @list: Variable type va_list
- * collaborative task by Mercy Chebet and Brian Murithi
- * Return: 0
- */
-int op_string(va_list list)
-{
-	char *s = va_arg(list, char*);
-	int count = 0;
-
-	if (s == NULL)
-		s = "(null)";
-
-	while (*s != '\0')
-		_putchar(*s);
-	s++;
-	count++;
-}
-
-return (count);
-}
-
-/**
- * op_integer - Function that prints integer or decimal
- * @list: Variable type va_list
- * Return: 0
- */
-int op_integer(va_list list)
-{
-	int number = va_arg(list, int);
-	unsigned int positive = 0;
-	int count = 0;
-
-	if (number < 0)
-	{
-		_putchar('-');
-		count++;
-		positive = number * (-1);
-	}
-	else
-	{
-		positive = number;
-	}
-
-	count += fnc_put_number_recursion(positive);
-
-	return (count);
-}
-
-/**
- * fnc_put_number_recursion - Function that prints the numbers with recursion
- * @number: Variable unsigned int type
- * Return: Count to the numbers prints
- */
-int fnc_put_number_recursion(unsigned int number)
-{
-	unsigned int r_mod, r_div;
-	int count = 0;
-
-	r_mod = number % 10;
-	r_div = number / 10;
-
-	if (r_div != 0)
-	{
-		count += (fnc_put_number_recursion(r_div));
-		count++;
-		_putchar(r_mod + '0');
-	}
-	else
-	{
-		_putchar(number + '0');
-		count++;
-	}
-
-	return (count);
-}
--- main.h
+++ main.h
@@ -1,9 +1,8 @@
-#ifndef PRINTF_H
-#define PRINTF_H
+fndef MAINN_H
+#define mainN_H
+#include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdarg.h>
-#include <stdio.h>
 /**
  *  * struct buffer - buffer structure for our implimentation of printf
  *   * @buf: buffer to write characters
@@ -17,103 +16,75 @@
  *           */
 typedef struct buffer
 {
-	  char *buf;
-	    char *tmpbuf;
-	      const char *format;
-	        va_list ap;
-		  int bp;
-		    int tp;
-		      int fp;
-		        unsigned int printed;
+		char *buf;
+			char *tmpbuf;
+				const char *format;
+					va_list ap;
+						int bp;
+							int tp;
+								int fp;
+									unsigned int printed;
 } buffer;
 /**
- *  * struct tags - Format tags after %
- *   * @spec: the specifier
- *    * @length: the length
- *     * @prec: the precision
- *      * @width: the width
- *       * @flags: the flags
- *        */
-typedef struct tags
+ *  *struct handlers - handlers struct
+ *   *@plus:'+' flag
+ *    *@space:' ' flag
+ *     *@hash:'#' flag
+ *      *@zero:'0' flag
+ *       *@minus:'-' handler
+ *        *@width:width handler
+ *         *@precision:precision handler
+ *          *@h_mod:h modifier handler
+ *           *@l_mod:l modifier handler
+ *            */
+typedef struct handlers
 {
-	  char spec;
-	    char length;
-	      int prec;
-	        int width;
-		  char flags[6];
-} tags;
+		unsigned int plus;
+			unsigned int space;
+				unsigned int hash;
+					unsigned int zero;
+						unsigned int minus;
+							unsigned int width;
+								unsigned int precision;
+									unsigned int h_mod;
+										unsigned int l_mod;
+} han_s;
 /**
- *  * struct parse_table - Table used for parsing the %s
- *   * @c: character found
- *    * @level: which level from 5 (specification) to 1 (flags)
- *     * @spec_func: function to put the matched specification into the buffer
+ *  * struct ops - a structure containing a char to compare with formatb modifiers
+ *   * and then choose the right function when it matches
+ *    * @operation: the char we want to compare to
+ *     * @func: the address of the function we want to return if a char matches
  *      */
-typedef struct parse_table
+typedef struct ops
 {
-	  char c;
-	    int level;
-	      void (*spec_func)();
-} parse_table;
-/* printf functions */
-void _copy(buffer *);
+		char operation;
+			int (*func)(va_list);
+} ops_f;
+
 int _printf(const char *format, ...);
-void _parse(buffer *b_r);
-void _init_tag(tags *t);
-void _init_buffer(buffer *b_r, const char *format);
-void _spec_c(buffer *b_r, tags *t);
-void _spec_s(buffer *b_r, tags *t);
-void _spec_nil(buffer *b_r);
-void _spec_pct(buffer *b_r);
-void _spec_p(buffer *b_r, tags *t);
-void _spec_r(buffer *b_r, tags *t);
-void _spec_S(buffer *b_r, tags *t);
-void _spec_o(buffer *b_r, tags *t);
-void _spec_u(buffer *b_r, tags *t);
-void _spec_x(buffer *b_r, tags *t);
-void _spec_X(buffer *b_r, tags *t);
-void _spec_b(buffer *b_r, tags *t);
-void _spec_d(buffer *b_r, tags *t);
-void _spec_R(buffer *b_r, tags *t);
-void error(void);
-int __atoi(const char *s, int n);
-/* _write_to_buffer functions */
+int _putchar(char c);
+int (*get_func(char ch))(va_list);
+int print_char(va_list list);
+int print_string(va_list list);
+int print_nan(char ch1, char ch2);
+int print_num(va_list list);
+unsigned int base_len(unsigned int num, int base);
+int print_binary(va_list list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_Hex(va_list list);
+int print_S(va_list list);
+int *rev_string(char *s);
+char *hex_conv(int num);
+int print_p(va_list list);
+int print_unsigned(va_list list);
+int print_rot13(va_list list);
 void _write(buffer *b_r, char c);
 void _write_str(buffer *b_r, char *s);
 void _write_tmpbuf(buffer *b_r);
-void _parse_tag(buffer *b_r, tags *t, parse_table *table);
-int str_len(char *str);
-void _revstr(char *s);
-char *_str_whelp(tags *t, char *hold, int hold_len);
-char *_to_hex_unreadable(char *hold);
-void _to_rot13(char *s);
-/* to string functions */
-char *_int_to_str(long int n);
-char *_int_to_hexstr(long int n);
-char *_int_to_caphexstr(long int n);
-char *_int_to_octstr(long int n);
-char *_int_to_binstr(long int n);
-/* unsigned to string functions */
-char *_uint_to_str(unsigned long int n);
-char *_uint_to_hexstr(unsigned long int n);
-char *_uint_to_caphexstr(unsigned long int n);
-char *_uint_to_octstr(unsigned long int n);
-char *_uint_to_binstr(unsigned long int n);
-/* printf_flag_helper functions */
-int _isFlagMinus(tags *t);
-int _isFlagPlus(tags *t);
-int _isFlagSpace(tags *t);
-int _isFlagHashtag(tags *t);
-int _isFlagZero(tags *t);
-/* parse helper functions */
-void _found_spec(buffer *b_r, tags *t, parse_table *table, int i);
-void _found_length(buffer *b_r, tags *t, parse_table *table, int i);
-void _found_prec(buffer *b_r, tags *t, parse_table *table, int i);
-void _found_width(buffer *b_r, tags *t);
-void _found_flag(buffer *b_r, tags *t, parse_table *table, int i);
-/spec num helper function/
-void _spec_num_help(buffer *b_r, tags *t, char *num_str, int minus);
-int num_len(int n);
-void get_sign(tags *t, int minus, char *front);
-char *check_prec(char *tmp_str, char *num_str, tags *t, int s_len);
-void _out_of_time(char *buf_str, char *tmp_str, char *front, tags *t);
-#endif
+int print_r(va_list list);
+
+
+
+
+#endif /*MAIN_H*/
