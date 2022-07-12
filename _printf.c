acd6dc45c72bf825c33e0fcf563c6593e9d6048a
MercyChebet <chebetm96@gmial.com>
2022-07-12 01:39:38 -0700

    Editing printf

--- _printf.c
+++ _printf.c
@@ -1,51 +1,140 @@
 #include "main.h"
 
 /**
- * _printf - prints formatted dara
- * @format: string that contains the format to print
- * @...: variadic functions
- * Return: number of characters written
+ * _printf - prints anything (output) according to a format
+ * @format: character string (c, s, %)
+ * Return: numbers of characters printed.
  */
-int _printf(char *format, ...)
-{
-	int written = 0, (*structype)(char *, va_list);
-	char q[3];
-	va_list pa;
 
-	if (format == NULL)
+int _printf(const char *format, ...)
+{
+	va_list list;
+	int i = 0, index = 0, count = 0, (*gof)(va_list list);
+
+	va_start(list, format);
+
+	if ((format == NULL) || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	q[2] = '\0';
-	va_start(pa, format);
-	_putchar(-1);
-	while (format[0])
+
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[0] == '%')
+		index = i + 1;
+		if (format[i] != '%')
+
 		{
-			structype = driver(format);
-			if (structype)
-			{
-				q[0] = '%';
-				q[1] = format[1];
-				written += structype(q, pa);
-			}
-			else if (format[1] != '\0')
-			{
-				written += _putchar('%');
-				written += _putchar(format[1]);
-			}
-			else
-			{
-				written += _putchar('%');
-				break;
-			}
-			format += 2;
+			_putchar(format[i]);
+			count++;
+		}
+		else if (format[i] == '%' && format[index] == '%')
+		{
+			_putchar('%');
+			i = index;
+			count++;
 		}
 		else
 		{
-			written += _putchar(format[0]);
-			format++;
+			gof = get_op_func(&format[index]);
+			if (gof != NULL)
+			{
+				count += gof(list);
+				i = index;
+			}
+			else
+			{
+				_putchar(format[i]);
+				count++;
+			}
 		}
 	}
-	_putchar(-2);
-	return (written);
+	va_end(list);
+	return (count);
+}
+
+/**
+ * op_char - Function that prints char
+ * @list: Variable type va_list
+ * Return: 0
+ */
+int op_char(va_list list)
+{
+	_putchar(va_arg(list, int));
+	return (1);
+}
+
+/**
+ * op_string - Function that prints string
+ * @list: Variable type va_list
+ * collaborative task by Mercy Chebet and Brian Murithi
+ * Return: 0
+ */
+int op_string(va_list list)
+{
+	char *s = va_arg(list, char*);
+	int count = 0;
+
+	if (s == NULL)
+		s = "(null)";
+
+	while (*s != '\0')
+		_putchar(*s);
+	s++;
+	count++;
+}
+
+return (count);
+}
+
+/**
+ * op_integer - Function that prints integer or decimal
+ * @list: Variable type va_list
+ * Return: 0
+ */
+int op_integer(va_list list)
+{
+	int number = va_arg(list, int);
+	unsigned int positive = 0;
+	int count = 0;
+
+	if (number < 0)
+	{
+		_putchar('-');
+		count++;
+		positive = number * (-1);
+	}
+	else
+	{
+		positive = number;
+	}
+
+	count += fnc_put_number_recursion(positive);
+
+	return (count);
+}
+
+/**
+ * fnc_put_number_recursion - Function that prints the numbers with recursion
+ * @number: Variable unsigned int type
+ * Return: Count to the numbers prints
+ */
+int fnc_put_number_recursion(unsigned int number)
+{
+	unsigned int r_mod, r_div;
+	int count = 0;
+
+	r_mod = number % 10;
+	r_div = number / 10;
+
+	if (r_div != 0)
+	{
+		count += (fnc_put_number_recursion(r_div));
+		count++;
+		_putchar(r_mod + '0');
+	}
+	else
+	{
+		_putchar(number + '0');
+		count++;
+	}
+
+	return (count);
 }
